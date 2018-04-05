f6004e374a3c12b82e902ca80c7dd2d5609f7070
ChamesBenJelloul <chames.benjelloul@gmail.com>
2018-04-06 00:17:26 +0100

    Table Personnel Migration+delete e-mail from users

--- database/migrations/2018_04_05_225957_create_personnels_table.php
+++ database/migrations/2018_04_05_225957_create_personnels_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreatePersonnelsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('personnels', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('username');
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('nom');
+            $table->string('prenom');
+            $table->string('matricule')->comment('departement');
+            $table->string('statut')->comment('actif/retraite/detachement/cooperation technique');
+
         });
     }
 
@@ -29,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('personnels');
     }
 }
