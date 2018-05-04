69d10a05a13777d256b3fe8584b12270a395b0d7
ChamesBenJelloul <chames.benjelloul@gmail.com>
2018-05-04 19:13:12 +0100

    exec php ping for tableaux de bords

--- resources/views/services/tableauxdebords.blade.php
+++ resources/views/services/tableauxdebords.blade.php
@@ -3,4 +3,23 @@
 @section('content')
 
     <h3>TABLEAUX DE BORDS</h3>
+<?php
+    //$ip =   "192.168.1.20";
+    //exec("ping -n 3 $ip", $output, $status);
+
+//echo exec("ping 192.168.1.20");
+$host="192.168.1.20";
+
+exec("ping -n 4 " . $host, $output, $result);
+
+print_r($output);
+
+if ($result == 0)
+
+    echo "Ping successful!";
+
+else
+
+    echo "Ping unsuccessful!";
+?>
 @endsection
