db640f42c61fc162347bc75794d8616aa1e65727
ChamesBenJelloul <chames.benjelloul@gmail.com>
2018-05-10 18:43:15 +0100

    home theme

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/custom1.css') }}" />
+    <link rel="shortcut icon" href="{{ asset('img/mylogo.ico') }}">
 </head>
 <body class="">
 <div class="login-page" >
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,6 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'ParcInformatique') }}</title>
-
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
